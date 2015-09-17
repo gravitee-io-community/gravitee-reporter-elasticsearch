d59135aa462a294ab0f6ce0f3180eeb789789f65
brasseld <brasseld@gmail.com>
2015-09-17 22:28:50 +0200

    refactor: Add new data to index from request / response

--- src/main/java/io/gravitee/reporter/elastic/engine/impl/AbstractElasticReportEngine.java
+++ src/main/java/io/gravitee/reporter/elastic/engine/impl/AbstractElasticReportEngine.java
@@ -15,11 +15,11 @@
  */
 package io.gravitee.reporter.elastic.engine.impl;
 
-import java.io.IOException;
-import java.net.InetAddress;
-import java.text.SimpleDateFormat;
-import java.util.TimeZone;
-
+import io.gravitee.common.http.GraviteeHttpHeader;
+import io.gravitee.gateway.api.Request;
+import io.gravitee.gateway.api.Response;
+import io.gravitee.reporter.elastic.config.Config;
+import io.gravitee.reporter.elastic.engine.ReportEngine;
 import org.elasticsearch.common.joda.time.format.DateTimeFormat;
 import org.elasticsearch.common.joda.time.format.DateTimeFormatter;
 import org.elasticsearch.common.xcontent.XContentBuilder;
@@ -28,10 +28,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import io.gravitee.gateway.api.Request;
-import io.gravitee.gateway.api.Response;
-import io.gravitee.reporter.elastic.config.Config;
-import io.gravitee.reporter.elastic.engine.ReportEngine;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
 
 
 public abstract class AbstractElasticReportEngine implements ReportEngine {
@@ -40,41 +41,41 @@ public abstract class AbstractElasticReportEngine implements ReportEngine {
 
 	@Autowired
 	private Config configuration;
-	  
+
 	/** Index simple date format **/
 	private SimpleDateFormat sdf;
-	    
+
 	/** Document simple date format **/
 	private  DateTimeFormatter dtf;
 
-	    
 	public AbstractElasticReportEngine(){
-		
 		this.sdf = new SimpleDateFormat("yyyy.MM.dd");
 		this.sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-		
+
 		this.dtf = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZZ");
 	}
-	    
-	
+
 	protected XContentBuilder getSource(Request request, Response response) throws IOException{
 		return XContentFactory.jsonBuilder()
-		.startObject()
-			.field("id", request.id())
-			.field("uri", request.uri())
-			.field("path", request.path())
-			.field("api-version", request.version())
-			.field("status", response.status())
-			.field("method", request.method().toString())
-			.field("request-content-type", request.contentType())
-			.field("response-content-type", response.headers().get("Content-Type"))		    
-			.field("request-content-length", request.contentLength() >= 0 ? request.contentLength() : null)
-			.field("response-content-length", response.headers().get("Content-Length"))					    
-			.field("hostname", InetAddress.getLocalHost().getHostName())
-			.field("@timestamp",request.timestamp(), dtf)
-		.endObject();
+				.startObject()
+				.field("id", request.id())
+				.field("uri", request.uri())
+				.field("path", request.path())
+				.field("status", response.status())
+				.field("method", request.method().toString())
+				.field("request-content-type", request.headers().contentType())
+				.field("response-content-type", response.headers().contentType())
+				.field("request-content-length", request.headers().contentLength() >= 0 ? request.headers().contentLength() : null)
+				.field("response-content-length", response.headers().contentLength() >= 0 ? response.headers().contentLength() : null)
+				.field("api-key", request.headers().getFirst(GraviteeHttpHeader.X_GRAVITEE_API_KEY))
+				.field("api-name", request.headers().getFirst(GraviteeHttpHeader.X_GRAVITEE_API_NAME))
+				.field("local-address", request.localAddress())
+				.field("remote-address", request.remoteAddress())
+				.field("hostname", InetAddress.getLocalHost().getHostName())
+				.field("@timestamp", Date.from(request.timestamp()), dtf)
+				.endObject();
 	}
-	
+
 	protected String getIndexName(Request request){
 		return String.format("%s-%s",configuration.getIndexName(), sdf.format(request.timestamp()));
 	}
--- src/main/java/io/gravitee/reporter/elastic/engine/impl/ElasticReportEngine.java
+++ src/main/java/io/gravitee/reporter/elastic/engine/impl/ElasticReportEngine.java
@@ -17,10 +17,12 @@ package io.gravitee.reporter.elastic.engine.impl;
 
 import java.io.IOException;
 import java.net.InetAddress;
+import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
 
+import io.gravitee.common.http.GraviteeHttpHeader;
 import io.gravitee.reporter.elastic.config.Config;
 import io.gravitee.reporter.elastic.engine.ReportEngine;
 import org.elasticsearch.action.bulk.BulkProcessor;
@@ -77,27 +79,28 @@ public class ElasticReportEngine implements ReportEngine {
 	 */
 	@Override
 	public void report(Request request, Response response) {
-			
 		try {
-			
 			String indexName =  String.format("%s-%s",configuration.getIndexName(), sdf.format(request.timestamp()));
 			
 			bulkProcessor.add(new IndexRequest(indexName, configuration.getTypeName()).source(
 				XContentFactory.jsonBuilder()
-					.startObject()
+						.startObject()
 						.field("id", request.id())
 						.field("uri", request.uri())
 						.field("path", request.path())
-						.field("api-version", request.version())
 						.field("status", response.status())
 						.field("method", request.method().toString())
-						.field("request-content-type", request.contentType())
-						.field("response-content-type", response.headers().get("Content-Type"))		    
-						.field("request-content-length", request.contentLength() >= 0 ? request.contentLength() : null)
-						.field("response-content-length", response.headers().get("Content-Length"))					    
+						.field("request-content-type", request.headers().contentType())
+						.field("response-content-type", response.headers().contentType())
+						.field("request-content-length", request.headers().contentLength() >= 0 ? request.headers().contentLength() : null)
+						.field("response-content-length", response.headers().contentLength() >= 0 ? response.headers().contentLength() : null)
+						.field("api-key", request.headers().getFirst(GraviteeHttpHeader.X_GRAVITEE_API_KEY))
+						.field("api-name", request.headers().getFirst(GraviteeHttpHeader.X_GRAVITEE_API_NAME))
+						.field("local-address", request.localAddress())
+						.field("remote-address", request.remoteAddress())
 						.field("hostname", InetAddress.getLocalHost().getHostName())
-						.field("@timestamp",request.timestamp(), dtf)
-					.endObject()));
+						.field("@timestamp", Date.from(request.timestamp()), dtf)
+						.endObject()));
 				
 		} catch (IOException e) {
 			LOGGER.error("Request {} report failed", request.id() ,e);
