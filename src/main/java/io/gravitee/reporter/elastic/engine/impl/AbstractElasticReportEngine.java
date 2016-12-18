9a06ec2629b6dab94f09c239b1cbe3b9fb850101
brasseld <brasseld@gmail.com>
2016-12-18 23:07:09 +0100

    feat(security): Add multiple security implementations and manage security by plan
    Closes gravitee-io/issues#379

--- src/main/java/io/gravitee/reporter/elastic/engine/impl/AbstractElasticReportEngine.java
+++ src/main/java/io/gravitee/reporter/elastic/engine/impl/AbstractElasticReportEngine.java
@@ -83,7 +83,7 @@ public abstract class AbstractElasticReportEngine implements ReportEngine {
 				.field("request-content-length", metrics.getRequestContentLength() >= 0 ? metrics.getRequestContentLength() : null)
 				.field("response-content-length", metrics.getResponseContentLength() >= 0 ? metrics.getResponseContentLength() : null)
 				.field("api-key", metrics.getApiKey())
-				.field("subscription", metrics.getSubscription())
+				.field("user", metrics.getUserId())
 				.field("plan", metrics.getPlan())
 				.field("api", metrics.getApi())
 				.field("application", metrics.getApplication())
